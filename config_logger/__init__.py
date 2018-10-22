e0c917d6788ee589f259c84d197d74441360d676
Afxentios Hadjiminas <afxentios.hadjiminas@bankofcyprus.com>
2018-10-22 22:24:52 +0300

    Updating the version

--- config_logger/__init__.py
+++ config_logger/__init__.py
@@ -2,5 +2,5 @@ from .filters import SameLevelFilter, LessEqualLevelFilter
 from .logger import Logger
 
 __all__ = ['Logger']
-__version__ = '1.0.2'
+__version__ = '1.1.0'
 __author__ = 'Afxentios Hadjiminas'
